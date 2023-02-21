2f47b723bab56092fcf998f7e28f30b8ad311fa9
localhostWebcoder <126019863+localhostWebcoder@users.noreply.github.com>
2023-02-22 01:45:06 +0530

    Browser detail code

---
+++ script.js
@@ -0,0 +1,19 @@
+const any = window.navigator;
+
+console.log(any.language);
+// output : en-IN
+
+console.log(any.appName);
+// Output : Netscape
+
+console.log(any.cookieEnabled);
+// Output : true
+
+console.log(any.onLine);
+// Output : true
+
+console.log(any.product);
+// Output : Gecko
+
+console.log(any.platform);
+// Output : Win32
